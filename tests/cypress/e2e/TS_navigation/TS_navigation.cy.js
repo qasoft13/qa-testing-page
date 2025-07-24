38702c2fd1544c52ab74a7e7651b66720ca8f4df
qasoft13 <qasoft13@gmail.com>
2025-07-24 18:42:37 -0400

    update Navigation

--- tests/cypress/e2e/TS_navigation/TS_navigation.cy.js
+++ tests/cypress/e2e/TS_navigation/TS_navigation.cy.js
@@ -1,61 +1,60 @@
-//import { CompaniesPage } from '../../support/pageObjects/CompaniesPage';
-import { CreateCompanyPage } from '../../support/pageObjects/CreateCompanyPage';
-import { CompaniesPage } from '../../support/pageObjects/CompaniesPage';
-import { CreatePropertyPage } from '../../support/pageObjects/CreatePropertyPage';
 import { utils } from '../../support/utils';
-import { Properties_Page } from '../../support/pageObjects/Properties_Page';
-
 
 describe('Navigation validation test suite', () => {
-
-  const companies = new CompaniesPage();
-  const company = new CreateCompanyPage();
-  const property = new CreatePropertyPage();
-  const properties_page = new Properties_Page();
+  
   const navBar = new utils();
   
+  const companyNameField = '#name';
+  const selectCompanyType = 'select';
+  const emailField = '#email';
+  const createCompanyButton =  '.btn-success';
+  const urlCompanyCreation = '/create-company';
+ 
+  const propertyNameFiled = '#name';
+  const addressField = '#address';
+  const priceField = '#price';
+  const sizeField = '#size';
+  const selectCompanyField = '#company';
+  const createButton = '.btn-success';
+  const backToListButton = '.btn-secondary ms-2';
+  const urlPropertyCreation = '/create-property';
 
-  beforeEach(function(){
+  const deleteButton = 'button.btn.btn-sm.btn-danger';
+   
 
-    company.goTo("http://localhost:5000/properties");
+  before(function(){
 
-  })//before each
+    cy.visit("http://localhost:5000/properties");
+
+  })
 
   it('TC_NAV_22 Nav Flow', function() {
-    /*
-    Title: TC_NAV_22 Nav Flow
-    Type: Functional
-    Priority: Hight
-    Module: Properties listings
-    Created By: Juan De los Rios
-    Date: 25-07-20
-    */
+    
 
     navBar.clickLeaseyaiLogo();
-    properties_page.clickCreateProperty();
-    properties_page.validatePropLandingPage();
-    property.clickCreateButton();
-    property.validateEmptyForm();
-    property.clickBackToListButton();
+    cy.url().should('include', '/properties');
+    cy.get('.btn-primary').click();
+    cy.contains('Create a New Property');
+    cy.get('.btn-secondary').click();
     navBar.clickCompaniesLogo();
-    companies.validateCompLandingPage();
-    company.clickCreateButton();
-    company.validateCompLandingPage();
-    company.clickCreateButton();
-    company.validateEmptyForm();
-    company.clickBackToListButton();
-    companies.validateCompLandingPage();
-    companies.deleteRandomCompany();
+    cy.contains('Company List');
+    cy.get(createCompanyButton).should('have.text', '+ Create Company');
+   // company.validateCompLandingPage();
+    //company.clickCreateButton();
+    //company.validateEmptyForm();
+   // company.clickBackToListButton();
+   // companies.validateCompLandingPage();
+   // companies.deleteRandomCompany();
     
-    })// fin NAV_01 Flow
+    })
 
 
   
 
 
 //reset data after run TS
-  after(function(){
-     navBar.resetData();
-    })
+ // after(function(){
+ //    navBar.resetData();
+  //  })
 
-})//fin describe suit
+})
